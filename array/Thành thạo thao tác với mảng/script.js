ad2747cfc22643739f730099f361d387774bb008
ngo ba kha <anhemdaica123@gmail.com>
2023-08-29 20:35:43 +0700

    your message

--- array/Thành thạo thao tác với mảng/script.js
+++ array/Thành thạo thao tác với mảng/script.js
@@ -5,62 +5,54 @@ for (let i = 1; i <= 5; i++) {
   numbers.push(number);
 }
 
-
-
 console.log(numbers);
 let numberlength = numbers.length;
 console.log("Lenght :" + numberlength);
 
-
-
-let sum = 0;
-for (let i = 0; i < numberlength; i++) {
-  sum += numbers[i];
-}
-console.log("Sum :" + sum);
-
-
-
+let sum = numbers[0];
 let max = numbers[0];
+let min = numbers[0];
+
 for (let j = 1; j < numberlength; j++) {
   if (numbers[j] > max) {
     max = numbers[j];
   }
-  console.log("Max :" + max);
-}
-
-
-
-let min = numbers[0];
-for (let j = 1; j < numberlength; j++) {
   if (numbers[j] < min) {
     min = numbers[j];
   }
+  sum += numbers[i];
 }
+
+console.log("Sum :" + sum);
+console.log("Max :" + max);
 console.log("Min :" + min);
 
-
-
 let newnumber = +prompt("Nhập số thêm mới");
-numbers.push(newnumber);
+numbers.unshift(newnumber);
 console.log(" Updated array (after adding a new number) : " + numbers);
 
+numbers.pop();
+console.log(" array sau khi da xoa :" + numbers);
 
-
-console.log (numbers.pop());
-console.log( " array sau khi da xoa :" + numbers );
-
-
-
-numbers.sort();
+numbers.sort((a, b) => a - b);
 let a = numbers.join(",");
 console.log("sap xep : " + a);
 
-let check = +prompt ("Nhập số muốn kiếm tra")
-for (let i = 0; i < numbers.length; i++){
-if (numbers[i] == check) {
-  console.log("Số " + numbers[i] + " có trong dãy và đứng tại vị trí số " + i);
-} else if (check ==! numbers[i]) { }
- console.log("khong tim thay" )
+let check = +prompt("Nhập số muốn kiếm tra");
 
+let index = -1;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] == check) {
+    index = i;
+    break;
+  }
+}
+
+if (index == -1) {
+  console.log("khong tim thay");
+} else {
+  console.log(
+    "Số " + numbers[index] + " có trong dãy và đứng tại vị trí số " + index
+  );
 }
